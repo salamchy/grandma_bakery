dc61ccfdfa36c52959bdcc303145ce72bf120948
salam <chaudharysm00@gmail.com>
2025-01-14 22:49:58 +0545

    pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+export default Home
